c6ab07d414b1c44683b7e343500ced6927272c73
Gerhard Kocher <gerhard.kocher@hokify.com>
2022-04-06 14:22:35 +0200

    mapped versions

--- src/server/api/versions.ts
+++ src/server/api/versions.ts
@@ -4,27 +4,48 @@ export default async (_req, _res) => {
 	const db = new Level('_versionsdata', { valueEncoding: 'json' });
 	await db.open();
 
-	const versions = {
-		git: null,
-		'test-worker': null,
-		'test-admin-app': null,
-		'prod-worker': null,
-		'prod-admin-app': null
-	};
-
 	try {
-		for (const env of Object.keys(versions)) {
+		/**
+		 * Init return values.
+		 */
+		const envs = [
+			'test-worker',
+			'test-admin-app',
+			'prod-worker',
+			'prod-admin-app'
+		];
+		const versions = {};
+
+		/**
+		 * Get stored versions from deployments.
+		 */
+		const deployed = {};
+		for (const env of envs) {
 			try {
-				versions[env] = await db.get(env);
+				deployed[env] = await db.get(env);
 			} catch (_e) {
-				versions[env] = {};
+				deployed[env] = {};
 			}
 		}
+
+		const gitVersions = await db.get('git');
+
+		/**
+		 * Map versions from deployments to modules.
+		 */
+		for (const [module, gitVersion] of Object.entries(gitVersions)) {
+			versions[module] = {
+				git: gitVersion
+			}
+			for (const env of envs) {
+				versions[module][env] = deployed[env][module];
+			}
+		}
+
+		return { envs, versions };
 	} catch (e) {
 		console.log(e);
 	} finally {
 		await db.close();
 	}
-
-	return versions;
 };
